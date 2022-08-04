17314141c0bfaa338f7221a08820b2c828703a6e
Alex Antonov <aantonov@cendyn.com>
2022-08-04 15:12:13 +0300

    useState example

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 import Calendar from "./Calendar";
 import Card from "../UI/Card";
 
@@ -5,14 +7,22 @@ import classes from './ExpenseItem.module.scss';
 
 
 const ExpenseItem = (props) => {
+
+    const [title, setTitle] = useState(props.title);
+
+    const buttonClickHandler = () => {
+        setTitle("Updated!");
+    }
+
+
     return (
         <Card className={classes.expenseItem}>
             <Calendar title={props.title} date={props.date} amount={props.amount}></Calendar>
             <div className={classes.description}>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className={classes.price}>${props.amount}</div>
             </div>
-            <button>Change title</button>
+            <button onClick={buttonClickHandler}>Change title</button>
         </Card>
     );
 }
