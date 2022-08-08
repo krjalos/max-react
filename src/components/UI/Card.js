d73b055e89d85ed3cd8bbadd5e83a27e0b56a944
Alex Antonov <aantonov@cendyn.com>
2022-08-08 23:59:43 +0300

    styled components

--- src/components/UI/Card.js
+++
@@ -1,8 +0,0 @@
-import classes from './Card.module.scss';
-
-const Card = (props) => {
-    const classAttr = classes.card + ' ' + props.className;
-    return <div className={classAttr}>{props.children}</div>
-}
-
-export default Card;
