c3cee978a81fe6db3e3b244122d52db14fcae9bb
Alex Antonov <aantonov@cendyn.com>
2022-08-09 17:29:19 +0300

    New Practice app

--- src/App.js
+++ src/App.js
@@ -1,42 +1,10 @@
-import Expenses from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense";
-import {useState} from "react";
+import React from 'react';
 
-const App = () => {
-    const INITIAL_EXPENSES = [
-        {
-            id: 'e1',
-            title: 'Toilet Paper1',
-            amount: 94.12,
-            date: new Date(2020, 7, 14),
-        },
-        {id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)},
-        {
-            id: 'e3',
-            title: 'Car Insurance',
-            amount: 294.67,
-            date: new Date(2021, 2, 28),
-        },
-        {
-            id: 'e4',
-            title: 'New Desk (Wooden)',
-            amount: 450,
-            date: new Date(2021, 5, 12),
-        },
-    ];
 
-    const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
-
-    const addExpenseHandler = (expense) => {
-        setExpenses((prevState) => {
-            return [expense, ...prevState];
-        });
-    }
-
+function App() {
   return (
     <div>
-        <NewExpense onAddExpense={addExpenseHandler}/>
-        <Expenses expenses={expenses}></Expenses>
+
     </div>
   );
 }
