97c43a852d2541623664d1dd8fa58a4781b20d6d
Alex Antonov <aantonov@cendyn.com>
2022-08-24 23:14:21 +0300

    http requests + error handling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React, {useState, useCallback} from 'react';
-import Button from "./components/UI/Button/Button";
+import React, {useState} from 'react';
 
+import MoviesList from './components/MoviesList';
 import './App.css';
-import Demo from "./components/Demo";
 
 function App() {
 
-    console.log("App runnning");
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-    const [showP, setShowP] = useState(false);
+  const fetchMoviesHandler = async () => {
+      setLoading(true);
+      setError(null);
 
-    const togglePHandler = useCallback (() => {
-        setShowP(prevState => !prevState);
-    }, []);
+      try {
+          const response = await fetch('https://swapi.dev/api/films');
+          if(!response.ok) {
+              throw new Error('Something went wrong');
+          }
+          const data = await response.json();
+
+          const transformedData = data.results.map(movie => {
+              return {
+                  id: movie.episode_id,
+                  title:movie.title,
+                  releaseDate:movie.release_date,
+                  openingText:movie.opening_crawl
+              };
+          });
+          setMovies(transformedData);
+
+      }catch(error) {
+          setError(error.message);
+      }
+      
+      setLoading(false);
+  }
 
   return (
-    <div className="app">
-      <h1>Hi there!</h1>
-        <Button onClick={togglePHandler}>Toggle P</Button>
-        <Demo show={showP}/>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+
+      <section>
+          {!loading && movies.length > 0 && <MoviesList movies={movies} />}
+          {!loading && movies.length < 1 && <p>No movies were loaded</p>}
+          {!loading && error && <p>{error}</p>}
+          {loading && <p>Movies are loading</p>}
+      </section>
+    </React.Fragment>
   );
 }
 
