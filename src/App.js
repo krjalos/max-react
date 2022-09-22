6624e4e28e65da9c161fa905e44101b69ae1ff44
Alex Antonov <aantonov@cendyn.com>
2022-09-22 14:29:16 +0300

    transition group package

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
+import {Transition} from "react-transition-group";
 
 class App extends Component {
 
   state = {
-    showModal : false
+    showModal: false, showSquare: false
   }
 
   closeModalHandler = () => {
-    this.setState({showModal : false });
+    this.setState({showModal: false});
   }
 
   showModalHandler = () => {
-    this.setState({showModal : true });
+    this.setState({showModal: true});
   }
 
   render() {
     console.log(this.state);
-    return (
-      <div className="App">
-        <h1>React Animations</h1>
-        <Modal show={this.state.showModal} closed={this.closeModalHandler} />
+    return (<div className="App">
+      <h1>React Animations</h1>
+      <button className='Button'
+              onClick={() => this.setState(prevState => ({showSquare: !prevState.showSquare}))}>Toggle square
+      </button>
+      <br/>
+
+      <Transition in={this.state.showSquare} timeout={300} mountOnEnter unmountOnExit>
+        {state => (<div style={{
+          background: 'red',
+          margin: '20px auto',
+          width: 100,
+          height: 100,
+          transition: 'all 1s ease-out',
+          opacity: state === 'exiting' ? 0 : 1
+        }}></div>)}
+      </Transition>
+
+      {this.state.showModal ? <React.Fragment>
+        <Modal show={this.state.showModal} closed={this.closeModalHandler}/>
         <Backdrop show={this.state.showModal}/>
-        <button onClick={this.showModalHandler} className="Button">Open Modal</button>
-        <h3>Animating Lists</h3>
-        <List />
-      </div>
-    );
+      </React.Fragment> : null}
+      <button onClick={this.showModalHandler} className="Button">Open Modal</button>
+      <h3>Animating Lists</h3>
+      <List/>
+    </div>);
   }
 }
 
