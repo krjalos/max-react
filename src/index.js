c54eb85e2e4349173ed0c90e8df52ce7077900bc
Alex Antonov <aantonov@cendyn.com>
2022-10-01 23:19:30 +0300

    Automated tests

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
-
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-import productStore from "./store/productStore";
 
-
-productStore();
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
